01b67c92831bdd411de33fa77595c6ff74b97c03
williamxyshi <williamxyshi@gmail.com>
2019-12-05 08:34:49 -0500

    implemented external libraries

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
           title: Text('true shoe'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
