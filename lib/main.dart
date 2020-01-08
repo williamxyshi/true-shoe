1d77667744ba6cb231b4f486a674c4900c28a50b
Will <williamxyshi@gmail.com>
2020-01-08 17:22:05 -0500

    ui for weather

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,50 @@
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWordsState extends State<RandomWords>{
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
+      title: 'Flutter Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('Flutter Weather App'),
+          ),
+          body: Center(
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text('New York', style: new TextStyle(color: Colors.white)),
+                          Text('Sunny', style: new TextStyle(color: Colors.white, fontSize: 32.0)),
+                          Text('72°F',  style: new TextStyle(color: Colors.white)),
+                          Image.network('https://openweathermap.org/img/w/01d.png'),
+                          Text('Jun 28, 2018', style: new TextStyle(color: Colors.white)),
+                          Text('18:30', style: new TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IconButton(
+                        icon: new Icon(Icons.refresh),
+                        tooltip: 'Refresh',
+                        onPressed: () => null,
+                        color: Colors.white,
+                      ),
+                    )
+                  ]
+              )
+          )
       ),
     );
   }
-
-}
-
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState() => RandomWordsState();
 }
