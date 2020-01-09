6775c785f6922df23e0fa15474e022b9adfb63e3
Will <williamxyshi@gmail.com>
2020-01-08 19:10:40 -0500

    openweatherapi and current location

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:true_shoe/weather.dart';
+import 'package:true_shoe/weatherdata.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
+
+//key 9b4c04c1a86c05ef46918fd12c8af1ce
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyAppState();
+  }
+}
+
+  class MyAppState extends State<MyApp> {
+    bool isLoading = false;
+    WeatherData weatherData;
+
+    Location _location = new Location();
+    String error;
+
+    @override
+    void initState() {
+      super.initState();
+
+      loadWeather();
+    }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Weather App',
+      title: 'True Shoe',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
           backgroundColor: Colors.blueGrey,
           appBar: AppBar(
-            title: Text('Flutter Weather App'),
+            title: Text('True Shoe'),
           ),
           body: Center(
               child: Column(
@@ -21,23 +52,17 @@ class MyApp extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text('New York', style: new TextStyle(color: Colors.white)),
-                          Text('Sunny', style: new TextStyle(color: Colors.white, fontSize: 32.0)),
-                          Text('72°F',  style: new TextStyle(color: Colors.white)),
-                          Image.network('https://openweathermap.org/img/w/01d.png'),
-                          Text('Jun 28, 2018', style: new TextStyle(color: Colors.white)),
-                          Text('18:30', style: new TextStyle(color: Colors.white)),
-                        ],
-                      ),
+                      child: weatherData != null ? Weather(weather: weatherData) : Container(),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: IconButton(
+                      child: isLoading ? CircularProgressIndicator(
+                        strokeWidth: 2.0,
+                        valueColor: new AlwaysStoppedAnimation(Colors.white),
+                      ) : IconButton(
                         icon: new Icon(Icons.refresh),
                         tooltip: 'Refresh',
-                        onPressed: () => null,
+                        onPressed: loadWeather,
                         color: Colors.white,
                       ),
                     )
@@ -47,4 +72,50 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+    loadWeather() async {
+      setState(() {
+        isLoading = true;
+      });
+
+      LocationData location;
+
+      try {
+        location = await  _location.getLocation();
+
+        error = null;
+      } on PlatformException catch (e) {
+        if (e.code == 'PERMISSION_DENIED') {
+          error = 'Permission denied';
+        } else if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
+          error = 'Permission denied - please ask the user to enable it from the app settings';
+        }
+
+        location = null;
+      }
+
+      if (location != null) {
+        final lat = location.latitude;
+        final lon = location.longitude;
+
+        final weatherResponse = await http.get(
+            'https://api.openweathermap.org/data/2.5/weather?APPID=9b4c04c1a86c05ef46918fd12c8af1ce&lat=${lat
+                .toString()}&lon=${lon.toString()}');
+
+        if (weatherResponse.statusCode == 200) {
+          return setState(() {
+            weatherData =
+            new WeatherData.fromJson(jsonDecode(weatherResponse.body));
+            isLoading = false;
+          });
+        }
+      }
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+
+
 }
